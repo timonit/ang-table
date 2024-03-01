d61c6c320a41a46880ef019e12b8295624c54a41
timonit <timonsus@gmail.com>
2024-03-01 17:57:46 +0500

    feature[companyAPI]: add data

--- src/entities/company/api/company.api.ts
+++ src/entities/company/api/company.api.ts
@@ -1,5 +1,9 @@
 import { API } from '@/shared/api/api';
 import { Company } from '../model/types';
+import data from './data';
 
 export class CompanyAPI extends API<Company> {
+  constructor() {
+    super(data);
+  }
 }
