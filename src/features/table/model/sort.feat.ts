c42a730f471f2e350259d9ab01ab8b971047b933
timonit <timonsus@gmail.com>
2024-03-04 02:34:54 +0500

    feature[sort]: add filter params

--- src/features/table/model/sort.feat.ts
+++ src/features/table/model/sort.feat.ts
@@ -6,12 +6,15 @@ import { Sort} from '@/shared/api/types';
 export async function sortFeat(sort: Sort<Company>) {
   console.log('start sort', sort);
   const companyAPI = new CompanyAPI();
-  const res = await companyAPI.get(1, sort);
+  const filter = companyStore.value.filter;
+  console.log('sort filter', filter)
+  const res = await companyAPI.get(1, sort, filter);
   companyStore.next({
     companies: res.list,
     length: res.length,
     countPage: res.pageCount,
     page: res.page,
     sort,
+    filter,
   });
 }
